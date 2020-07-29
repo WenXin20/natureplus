5c4255b6f31ebb1626a13dd342f8d75e51bd2e0e
WenXin20 <newvi@ASUS-Laptop.lan>
2020-07-28 19:10:08 -0500

    -Added fence gates -Added doors to the wooden_doors tag

---
+++ src/main/java/net/wenxin/natureplus/fuel/AzureFenceGateFuelFuel.java
@@ -0,0 +1,25 @@
+
+package net.wenxin.natureplus.fuel;
+
+import net.wenxin.natureplus.block.AzureFenceGateBlock;
+import net.wenxin.natureplus.NatureplusModElements;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.item.ItemStack;
+
+@NatureplusModElements.ModElement.Tag
+public class AzureFenceGateFuelFuel extends NatureplusModElements.ModElement {
+	public AzureFenceGateFuelFuel(NatureplusModElements instance) {
+		super(instance, 804);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public void furnaceFuelBurnTimeEvent(FurnaceFuelBurnTimeEvent event) {
+		if (event.getItemStack().getItem() == new ItemStack(AzureFenceGateBlock.block, (int) (1)).getItem())
+			event.setBurnTime(300);
+	}
+}
